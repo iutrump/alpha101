88a665be9e760757b44d4690fbc7b28e0dd09a09
Siming Chen <867965859@qq.com>
2026-05-31 14:17:53 +0800

    Remove legacy data compatibility modules

--- alpha101/data/alpha_view.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from alpha101.data.views import Alphas, FactorDataView, LazyPolarsFactor, PolarsFactor, PolarsLongDataView
-
-__all__ = ["Alphas", "FactorDataView", "LazyPolarsFactor", "PolarsFactor", "PolarsLongDataView"]
